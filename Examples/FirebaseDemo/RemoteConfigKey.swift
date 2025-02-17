5f71b7886936ec6c68788b97f8f6e595313329eb
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2025-02-17 09:10:53 -0800

    Add dependency client for remote config. (#112)
    * Add dependency client for remote config.
    
    * Update RemoteConfigKey.swift
    
    * Update RemoteConfigKey.swift
    
    * preconcurrency import
    
    ---------
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>

--- Examples/FirebaseDemo/RemoteConfigKey.swift
+++ Examples/FirebaseDemo/RemoteConfigKey.swift
@@ -1,3 +1,4 @@
+@preconcurrency import Combine
 import Dependencies
 @preconcurrency import FirebaseRemoteConfig
 import Sharing
@@ -10,39 +11,108 @@ extension SharedReaderKey {
 
 struct RemoteConfigKey<Value: Decodable & Sendable>: SharedReaderKey {
   let key: String
+  let remoteConfig: any RemoteConfigClient
+
+  init(key: String) {
+    self.key = key
+    @Dependency(\.remoteConfig) var remoteConfig
+    self.remoteConfig = remoteConfig
+  }
+
   var id: some Hashable { key }
-  @Dependency(RemoteConfigDependencyKey.self) var remoteConfig
+
   func load(context _: LoadContext<Value>, continuation: LoadContinuation<Value>) {
-    remoteConfig.fetchAndActivate { changed, error in
-      if let error {
-        continuation.resume(throwing: error)
-      } else {
-        continuation.resume(with: Result { try remoteConfig.configValue(forKey: key).decoded() })
-      }
-    }
+    remoteConfig.fetch(key: key, completion: continuation.resume(with:))
   }
   func subscribe(
     context _: LoadContext<Value>, subscriber: SharedSubscriber<Value>
   ) -> SharedSubscription {
-    let registration = remoteConfig.addOnConfigUpdateListener { update, error in
-      guard error == nil else { return }
-      remoteConfig.activate { changed, error in
-        guard error == nil else { return }
-        subscriber.yield(with: Result { try remoteConfig.configValue(forKey: key).decoded() })
-      }
-    }
+    let cancellable = remoteConfig.addUpdateListener(
+      key: key,
+      subscriber: subscriber.yield(with:)
+    )
     return SharedSubscription {
-      registration.remove()
+      cancellable.cancel()
     }
   }
 }
 
-private enum RemoteConfigDependencyKey: DependencyKey {
-  public static var liveValue: RemoteConfig {
-    let remoteConfig = RemoteConfig.remoteConfig()
+protocol RemoteConfigClient: Sendable {
+  func fetch<T: Decodable>(
+    key: String,
+    completion: @escaping (Result<T, any Error>) -> Void
+  )
+  func addUpdateListener<T: Decodable>(
+    key: String,
+    subscriber: @escaping (Result<T, any Error>) -> Void
+  ) -> AnyCancellable
+}
+
+private enum RemoteConfigClientKey: DependencyKey {
+  static var liveValue: any RemoteConfigClient {
+    FirebaseRemoteConfig()
+  }
+}
+
+extension DependencyValues {
+  var remoteConfig: any RemoteConfigClient {
+    get { self[RemoteConfigClientKey.self] }
+    set { self[RemoteConfigClientKey.self] = newValue }
+  }
+}
+
+struct FirebaseRemoteConfig: RemoteConfigClient {
+  let remoteConfig = RemoteConfig.remoteConfig()
+  init() {
     let settings = RemoteConfigSettings()
     settings.minimumFetchInterval = 0
     remoteConfig.configSettings = settings
-    return remoteConfig
   }
+  func fetch<T: Decodable>(key: String, completion: @escaping (Result<T, any Error>) -> Void) {
+    remoteConfig.fetchAndActivate { _, error in
+      completion(
+        Result {
+          try remoteConfig.configValue(forKey: key).decoded()
+        }
+      )
+    }
+  }
+  func addUpdateListener<T: Decodable>(
+    key: String,
+    subscriber: @escaping (Result<T, any Error>) -> Void
+  ) -> AnyCancellable {
+    let registration = remoteConfig.addOnConfigUpdateListener { _, error in
+      guard error == nil else { return }
+      remoteConfig.activate { changed, error in
+        guard error == nil else { return }
+        subscriber(Result { try remoteConfig.configValue(forKey: key).decoded() })
+      }
+    }
+    return AnyCancellable { registration.remove() }
+  }
+}
+
+final class MockRemoteConfig: RemoteConfigClient {
+  let config: [String: any Sendable]
+  init(config: [String: any Sendable]) {
+    self.config = config
+  }
+  func fetch<T: Decodable>(
+    key: String,
+    completion: @escaping (Result<T, any Error>) -> Void
+  ) {
+    guard let value = config[key] as? T
+    else {
+      completion(.failure(NotFound()))
+      return
+    }
+    completion(.success(value))
+  }
+  func addUpdateListener<T: Decodable>(
+    key: String,
+    subscriber: @escaping (Result<T, any Error>) -> Void
+  ) -> AnyCancellable {
+    AnyCancellable {}
+  }
+  struct NotFound: Error {}
 }
