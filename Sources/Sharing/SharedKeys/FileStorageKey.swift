79fe301424c7d2050d706b1e3e5e1d247e7a5564
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2025-03-18 12:13:20 -0700

    Update Sources/Sharing/SharedKeys/FileStorageKey.swift
    Co-authored-by: Pyry Jahkola <pyry.jahkola@iki.fi>

--- Sources/Sharing/SharedKeys/FileStorageKey.swift
+++ Sources/Sharing/SharedKeys/FileStorageKey.swift
@@ -379,8 +379,8 @@
           close(source.handle)
         }
       },
-      load: { url in try
-        Data(contentsOf: url)
+      load: { url in
+        try Data(contentsOf: url)
       },
       save: { data, url in
         try data.write(to: url, options: .atomic)
